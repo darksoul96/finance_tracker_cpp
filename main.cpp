4f7d340b65cc3eb38aafc8d69fcb6057081c6aed
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2024-06-25 20:19:15 -0300

    Created basic window

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 
-
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main (int argc, char *argv[]) {
-  
-  std::cout << "hello world";
+  // Initialize GLFW
+  if (!glfwInit()) {
+      std::cerr << "Failed to initialize GLFW" << std::endl;
+      return -1;
+  } 
+
+  GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
 
   return 0;
 }
