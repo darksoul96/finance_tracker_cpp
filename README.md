7da8fff32fff13b44ff9555d4ffb2c534df10a55
Agustin F. Ruiz <62730262+darksoul96@users.noreply.github.com>
2024-06-26 00:19:08 -0300

    Added sqlite3 static files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # finance_tracker_cpp
+
+## CMake build command
+
+### run inside /build directory
+
+`cmake -S .. -B . -DCMAKE_EXPORT_COMPILE_COMMANDS=ON`
--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <sqlite3.h>
 
-int main (int argc, char *argv[]) {
-  // Initialize GLFW
+int main(int argc, char *argv[]) {
   if (!glfwInit()) {
-      std::cerr << "Failed to initialize GLFW" << std::endl;
-      return -1;
-  } 
+    std::cerr << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
 
-  GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+  GLFWwindow *window =
+      glfwCreateWindow(1080, 720, "Finance Tracker", NULL, NULL);
 
   if (!window) {
     glfwTerminate();
@@ -23,7 +24,6 @@ int main (int argc, char *argv[]) {
 
   glfwDestroyWindow(window);
   glfwTerminate();
-
 
   return 0;
 }
